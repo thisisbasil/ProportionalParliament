0d47ba67774e2be0464c248c1015d6d2ee24e7b8
Basil Huffman <bahuffma@gwmail.gwu.edu>
2017-06-12 21:17:16 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <cctype>
+#include <sstream>
+#include <algorithm>
+#include "parseat.h"
+#include "circprop.h"
+using namespace std;
+
+int getNumSeats(ifstream& file)
+{
+	string line;
+	while(getline(file,line))
+	{
+		int temp = atoi(line.c_str());
+		if (temp > 0)
+			return temp;
+	}
+
+	return -1;	
+}
+
+bool skipline(string line)
+{
+	if (line[0] == '#' || line[0] == '\0' || isspace(line[0]))	
+		return true;
+	return false;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		cout << "format: parseat <input file>" << endl;
+		return 0;	
+	}
+	
+	ifstream file;
+	file.open(argv[1]);
+	if (file.is_open())
+	{
+		int numseats = getNumSeats(file);
+		parseat model(numseats);
+		
+		string line;
+		while(getline(file,line))
+		{
+			string name;
+			long numvotes;
+			if(skipline(line))
+				continue;
+			istringstream iss(line);
+			iss >> name;
+			iss >> numvotes;
+			model.add(name,numvotes);	
+		}
+		model.setStrategy(new CircProp);
+		model.runModel();
+		model.printResults();
+	}
+	else
+	{
+		cout << argv[1] << " is not present." << endl;
+		return 1;	
+	}
+	return 1;
+}
