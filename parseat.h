fa26511c6ccefda49c949a519e5e7d7e9cd2c49d
Basil Huffman <bahuffma@gwmail.gwu.edu>
2017-06-12 21:17:47 -0400

    Create parseat.h

---
+++ parseat.h
@@ -0,0 +1,44 @@
+#ifndef PARSEAT_H
+#define PARSEAT_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <cctype>
+#include <sstream>
+#include <algorithm>
+#include "party.h"
+#include "strategy.h"
+using namespace std;
+
+class parseat
+{
+private:
+	double threashold;
+	vector<party> parties;
+	int numseats;
+	StrategyBase* strategy;
+
+public:
+	parseat(double);
+	parseat(int);
+	parseat(double, int);
+	parseat();
+
+	void add(string,long);
+	void clear();
+	void printResults();
+
+	void setNumSeats(int);
+	int getNumSeats();
+	double getThreashold();
+	void setThreashold(double);
+	void setStrategy(StrategyBase*);
+	void runModel();
+	int getNumParties() { return parties.size(); }
+};
+
+#endif
