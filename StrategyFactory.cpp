83c078930dafcd811aa269622f0eb8214ca23e2f
Basil Huffman <bahuffma@gwmail.gwu.edu>
2017-07-31 10:33:41 -0400

    Update StrategyFactory.cpp

--- StrategyFactory.cpp
+++ StrategyFactory.cpp
@@ -3,9 +3,9 @@
 shared_ptr<StrategyBase> StrategyFacory::getStrategy(string _strategy)
 {
 	shared_ptr<StrategyBase> retval;
-	for(unsigned int i=0; i< _strategy.length(); i++)
+	for(auto i=0; i< _strategy.length(); i++)
 	{
-		char c = _strategy[i];
+		auto c = _strategy[i];
 		c = toupper(c);
 		_strategy[i] = c;
 	}
